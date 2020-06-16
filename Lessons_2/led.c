3a6dcba63a37dea9f6fab4389dfc64d274dee521
Wei.Ding <273211367@qq.com>
2020-06-16 21:33:23 +0800

    对led.c进行了修正

--- Lessons_2/led.c
+++ Lessons_2/led.c
@@ -1,19 +1,19 @@
 /*LED_Flash*/
-int led_cnt = 7;
+int led_cnt = 0;
 
 void setup()
 {
-  for(led_cnt; led_cnt<=7; led_cnt++){
-  	pinMode(led_cnt, OUTPUT);
-  }
+	for(led_cnt; led_cnt<=7; led_cnt++){
+		pinMode(led_cnt, OUTPUT);
+	}
 }
 
 void loop()
 {
-  for(led_cnt; led_cnt>=0; led_cnt--){
-  	digitalWrite(led_cnt, HIGH);
-    delay(500);
-    digitalWrite(led_cnt, LOW);
+	led_cnt = 7;
+	for(led_cnt; led_cnt>=0; led_cnt--){
+		digitalWrite(led_cnt, HIGH);
+		delay(500);
+		digitalWrite(led_cnt, LOW);
   }
-  led_cnt = 7;
 }
